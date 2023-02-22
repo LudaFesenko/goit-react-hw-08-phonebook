c1047feae47f2aa88f2d5ee9b46665a8f6dd554f
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-02-22 19:41:23 +0200

    add Loyout

---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Layout = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+  padding: 16px;
+  background-color: #cccccc;
+  text-align: center;
+`;
