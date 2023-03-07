24d5cd7984788af2714da63841b771be8f9f92d5
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-03-07 13:38:30 +0200

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import { Layout } from './Layout';
@@ -6,16 +6,33 @@ import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
 
-const initialState = [
+const STORE_KEY = 'contacts';
+
+const INITIAL_STATE = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
+const getInitialContacts = () => {
+  const savedContacts = localStorage.getItem(STORE_KEY);
+
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
+  }
+
+  return INITIAL_STATE;
+};
+
 function App() {
-  const [contacts, setContacts] = useState(initialState);
   const [filter, setFilter] = useState('');
+
+  const [contacts, setContacts] = useState(getInitialContacts);
+
+  useEffect(() => {
+    localStorage.setItem(STORE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = (name, number) => {
     const existed = contacts.find(contact => contact.name === name);
