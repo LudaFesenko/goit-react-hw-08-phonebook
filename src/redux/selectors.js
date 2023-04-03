f54563e70c106de10da0a069d5868e6ac8db08c3
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-04-03 20:54:41 +0300

    add selectors

---
+++ src/redux/selectors.js
@@ -0,0 +1,13 @@
+export const selectFilter = state => state.filter.filter;
+export const selectContacts = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+
+export const selectFilteredContacts = state => {
+  const contacts = selectContacts(state);
+  const filter = selectFilter(state).toLowerCase().trim();
+
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().trim().includes(filter)
+  );
+};
