648f17bcfc28a60af6726a66ba6d8bc3bf46da3f
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-04-14 18:04:52 +0300

    remove files

--- src/components/Layout/Layout.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Layout = styled.div`
-  width: 1200px;
-  margin: 0 auto;
-  padding: 16px;
-  background-color: #cccccc;
-  text-align: center;
-`;
