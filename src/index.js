e7c9ae62e673f0716332f0f074ebfa72002d64d5
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-04-14 18:06:40 +0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import { App } from 'components/App';
+import { persistor, store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <CssBaseline />
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
